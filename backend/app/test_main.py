5c16b573c00a857e0308fdfac7419d25cac7a898
Imad-C <imadchowdhury01@gmail.com>
2025-12-06 23:11:35 +0000

    Test /tasks GET request

--- backend/app/test_main.py
+++ backend/app/test_main.py
@@ -8,14 +8,7 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
 
-
 def test_create_task():
-    payload = {
-        "title": "Test Task",
-        "description": "Test Task Description",
-        "status": "todo",
-        "due": "2025-01-01T12:00:00"
-        }
     response = client.post(
         "/tasks", 
         headers={"Content-Type": "application/json"}, 
@@ -35,5 +28,16 @@ def test_create_task():
         "status": "todo",
         "due": "2025-01-01T12:00:00"
         }
+    
+def test_read_tasks():
+    response = client.get('/tasks')
+    assert response.status_code == 200
+    assert response.json() == [{
+        "id": 1,
+        "title": "Test Task",
+        "description": "Test Task Description",
+        "status": "todo",
+        "due": "2025-01-01T12:00:00"
+        }]
     
     
